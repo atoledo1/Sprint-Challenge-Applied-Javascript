85ea71e45ff0c847de03d8d59de78fcf3c12df8d
adriana <atoledosantiago1@gmail.com>
2020-05-08 16:58:58 -0500

    mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -2,7 +2,7 @@
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
 // Study the response data you get back, closely.
-// You will be creating a card for each 'article' in the response.
+// You will be creating a card for each 'article' in the info.
 // This won't be as easy as just iterating over an array though.
 //
 // Write a function that returns the following markup:
@@ -21,75 +21,56 @@
 
 
 
-axios.get('https://lambda-times-backend.herokuapp.com/articles')
-.then( response => {
-	const javascript = response.data.articles.javascript;
-	javascript.forEach(article =>{
-	  	articleComponent(article); 	
-	  	
-	})	
+function cardCreator(article){
 
-	const bootstrap = response.data.articles.bootstrap;
- 	bootstrap.forEach(boot =>{
- 	  	articleComponent(boot); 
- 	  		
- 	})
 
- 	const technology = response.data.articles.technology;
- 	technology.forEach(tech =>{
- 	  	articleComponent(tech); 
- 	  		
- 	})	
+const articleBox = document.createElement("div");
+const title = document.createElement("div");
+const authorD = document.createElement("div");
+const imageDiv = document.createElement("div");
+const photo = document.createElement("img");
+const nameA = document.createElement("span");
 
- 	const jquery = response.data.articles.jquery;
- 	jquery.forEach(jquery =>{
- 	  	articleComponent(jquery); 
- 	  		
- 	})
 
- 	const node = response.data.articles.node;
- 	node.forEach(node =>{
- 	   	articleComponent(node); 
- 		
- 	})														
-	
-}) 
-.catch(error => {
-	console.log('error', error);
+articleBox.classList.add("card");
+title.classList.add("headline");
+authorD.classList.add("author");
+imageDiv.classList.add("img-container");
+
+
+title.textContent = article.headline;
+photo.src = article.authorPhoto;
+nameA.textContent = `By ${article.authorName}`;
+
+
+
+articleBox.appendChild(title);
+articleBox.appendChild(authorD);
+authorD.appendChild(imageDiv);
+imageDiv.appendChild(photo);
+authorD.appendChild(nameA);
+
+
+return articleBox;
+
+};
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles").then(info => { 
+console.log(info);
+
+const cardsBox = document.querySelector(".cards-container");
+const arrayNew = Object.values(info.data.articles);
+
+arrayNew.forEach((element) => { 
+element.forEach((article) => {
+    cardsBox.appendChild(cardCreator(article));
+    
 })
 
-	
-function articleComponent(response){
-	response = response;
-	const cardsContainer = document.querySelector('.cards-container');
-    
-    
-	const card = document.createElement('div');
-	const divHeadline = document.createElement('div');
-	const divAuthor = document.createElement('div');
-	const divImg = document.createElement('img');
-	const span = document.createElement('span');
+})
 
-	
-	card.classList.add('card');
-	divHeadline.classList.add('headline');
-	divAuthor.classList.add('author');
-	divImg.classList.add('img-container');
+}).catch(error => {
+console.log("error", error)
 
-	
-	divHeadline.textContent = `${response['headline']}`;
-	divImg.src = `${response['authorPhoto']}`;
-	span.textContent = `${response['authorName']}`;
-
-	
-		card.appendChild(divHeadline);
-		card.appendChild(divAuthor);
-		divAuthor.appendChild(divImg);
-		divAuthor.appendChild(span);
-
-		return cardsContainer.appendChild(card);
-
-}
-
-
+});  
 
